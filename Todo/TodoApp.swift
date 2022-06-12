eb8704e2c138c3c046c1c231e187003a80f9c080
mmm1379 <mmmasoudpour@gmail.com>
2022-06-12 21:04:19 +0430

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Mohammad Mahdi Masoudpour on 3/22/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
